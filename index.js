9313146083d6f6683a9fb7378d1b198922f981b5
Khawaja Abdul Hafeez <54060296+Khhafeez47@users.noreply.github.com>
2024-10-05 11:44:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
+// Route to handle the default page
 app.get('/', function(request, response) {
-  response.send('Hello World this is from test branch!')
-})
+  response.send('Hello World this is from test branch!');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// Listen on all interfaces (0.0.0.0), not just localhost
+app.listen(app.get('port'), '0.0.0.0', function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
